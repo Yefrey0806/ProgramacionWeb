fc3f6f386179e724b0890e55359276b9dd9967c7
Yefrey0806 <sanjuanferia@gmail.com>
2025-09-17 09:41:29 -0500

    React

---
+++ ReactV2/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1>Hola Mundo</h1>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ ReactV2/src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PhoneIphoneIcon from "@mui/icons-material/PhoneIphone";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <PhoneIphoneIcon id="icono-header" />
+      <h1>Ejercicio de React</h1>
+    </header>
+  );
+};
+
+export default Header;
