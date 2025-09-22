e8d3434db1997644a55738a153d1b15dc44afbdb
Yefrey0806 <sanjuanferia@gmail.com>
2025-09-22 07:13:45 -0500

    React N

--- ReactV2/src/Components/CardSection/CardSection.jsx
+++ ReactV2/src/Components/CardSection/CardSection.jsx
@@ -1,27 +1,26 @@
-import * as React from "react";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import lagarto from "../../assets/lagarto.jpg";
-import "./CardSection.css";
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import lagarto from '../../assets/lagarto.jpg';
+import './CardSection.css';
 
 export default function CardSection() {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
-        sx={{ height: 140 }}
+        component="img"
+        alt="green iguana"
         image={lagarto}
-        imageAlt="green iguana"
-        title="green iguana"
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           Lizard
         </Typography>
-        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
           Lizards are a widespread group of squamate reptiles, with over 6,000
           species, ranging across all continents except Antarctica
         </Typography>
@@ -33,3 +32,4 @@ export default function CardSection() {
     </Card>
   );
 }
+
